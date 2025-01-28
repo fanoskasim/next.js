54850133dbd79e14c389d7b1d0dab50a9612f3e3
Janka Uryga <lolzatu2@gmail.com>
2025-01-28 17:09:00 +0100

    replace long if-chains with a set

--- crates/next-custom-transforms/src/transforms/track_dynamic_imports.rs
+++ crates/next-custom-transforms/src/transforms/track_dynamic_imports.rs
@@ -1,5 +1,8 @@
+use indexmap::{indexset, IndexSet};
+use lazy_static::lazy_static;
 use serde::Deserialize;
 use swc_core::{
+    atoms::Atom,
     common::{comments::Comments, util::take::Take, Span, SyntaxContext},
     ecma::{
         ast::*,
@@ -26,14 +29,7 @@ struct ImportReplacer<C> {
     track_dynamic_import_local_ident: Ident,
     track_async_function_local_ident: Ident,
     has_dynamic_import: bool,
-    has_global: TrackedGlobals,
-}
-
-struct TrackedGlobals {
-    webpack_load: bool,
-    webpack_require: bool,
-    turbopack_load: bool,
-    turbopack_require: bool,
+    identifiers_to_instrument: indexmap::IndexSet<Atom>,
 }
 
 impl<C> ImportReplacer<C>
@@ -47,14 +43,18 @@ where
             track_dynamic_import_local_ident: private_ident!("$$trackDynamicImport__"),
             track_async_function_local_ident: private_ident!("$$trackAsyncFunction__"),
             has_dynamic_import: false,
-            has_global: TrackedGlobals {
-                webpack_load: false,
-                webpack_require: false,
-                turbopack_load: false,
-                turbopack_require: false,
-            },
+            identifiers_to_instrument: IndexSet::new(),
         }
     }
+}
+
+lazy_static! {
+    static ref GLOBALS_TO_INSTRUMENT: IndexSet<Atom> = indexset! {
+        "__webpack_load__".into(),
+        "__webpack_require__".into(),
+        "__turbopack_load__".into(),
+        "__turbopack_require__".into(),
+    };
 }
 
 impl<C> VisitMut for ImportReplacer<C>
@@ -113,22 +113,8 @@ where
         };
 
         let mut needs_track_async_function = false;
-        if self.has_global.webpack_load {
-            insert_global_fn_wrapper(stmts, "__webpack_load__");
-            needs_track_async_function = true;
-        }
-
-        if self.has_global.webpack_require {
-            insert_global_fn_wrapper(stmts, "__webpack_require__");
-            needs_track_async_function = true;
-        }
-
-        if self.has_global.turbopack_load {
-            insert_global_fn_wrapper(stmts, "__turbopack_load__");
-            needs_track_async_function = true;
-        }
-        if self.has_global.turbopack_require {
-            insert_global_fn_wrapper(stmts, "__turbopack_require__");
+        for name in &self.identifiers_to_instrument {
+            insert_global_fn_wrapper(stmts, name.as_str());
             needs_track_async_function = true;
         }
 
@@ -165,21 +151,13 @@ where
     }
 
     fn visit_mut_ident(&mut self, ident: &mut Ident) {
-        // find references to bundler globals
+        // find references to bundler globals like `__turbopack_load__`
         //
         // "globals" like this use the unresolved syntax context
         // https://rustdoc.swc.rs/swc_core/ecma/transforms/base/fn.resolver.html#unresolved_mark
         // if it's not unresolved, then there's a local redefinition which we don't want to touch
-        if ident.ctxt == self.unresolved_ctxt {
-            if ident.sym == "__webpack_load__" {
-                self.has_global.webpack_load = true;
-            } else if ident.sym == "__webpack_require__" {
-                self.has_global.webpack_require = true;
-            } else if ident.sym == "__turbopack_load__" {
-                self.has_global.turbopack_load = true;
-            } else if ident.sym == "__turbopack_require__" {
-                self.has_global.turbopack_require = true;
-            }
+        if ident.ctxt == self.unresolved_ctxt && GLOBALS_TO_INSTRUMENT.contains(&ident.sym) {
+            self.identifiers_to_instrument.insert(ident.sym.clone());
         }
     }
 }
