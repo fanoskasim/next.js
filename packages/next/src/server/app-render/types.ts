2dcfcddbe3748054ba2d8e7c3f542e2b5bed0fca
Jiachi Liu <inbox@huozhi.im>
2025-03-24 20:38:34 -0700

    polish styling

--- packages/next/src/server/app-render/types.ts
+++ packages/next/src/server/app-render/types.ts
@@ -163,6 +163,7 @@ export type ServerOnInstrumentationRequestError = (
 ) => void | Promise<void>
 
 export interface RenderOptsPartial {
+  dir?: string
   previewProps: __ApiPreviewProps | undefined
   err?: Error | null
   dev?: boolean
