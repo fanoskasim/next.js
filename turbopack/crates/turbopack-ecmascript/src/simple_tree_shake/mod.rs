ee2dd353d84d26c40afb99ecfef69251d8102275
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-15 15:03:19 +0900

    is_export_used: Downcast

--- turbopack/crates/turbopack-ecmascript/src/simple_tree_shake/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/simple_tree_shake/mod.rs
@@ -3,12 +3,22 @@
 use anyhow::Result;
 use turbo_rcstr::RcStr;
 use turbo_tasks::{ResolvedVc, Vc};
+use turbopack_core::module::Module;
 
-use crate::chunk::EcmascriptChunkPlaceable;
+use crate::{chunk::EcmascriptChunkPlaceable, references::esm::EsmAssetReference};
 
 #[turbo_tasks::function]
 pub async fn is_export_used(
     module: ResolvedVc<Box<dyn EcmascriptChunkPlaceable>>,
     export_name: RcStr,
 ) -> Result<Vc<bool>> {
+    let references = module.references();
+
+    for &reference in references.await?.iter() {
+        let Some(reference) = ResolvedVc::try_downcast_type::<EsmAssetReference>(reference) else {
+            continue;
+        };
+    }
+
+    Ok(Vc::cell(true))
 }
