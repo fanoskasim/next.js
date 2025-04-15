8ae81dd4d0ca345f7e436463e873f94f5bff1f82
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-15 15:00:57 +0900

    `is_export_used`

--- turbopack/crates/turbopack-ecmascript/src/simple_tree_shake/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/simple_tree_shake/mod.rs
@@ -1,1 +1,14 @@
 //! Intermediate tree shaking that uses global information but not good as the full tree shaking.
+
+use anyhow::Result;
+use turbo_rcstr::RcStr;
+use turbo_tasks::{ResolvedVc, Vc};
+
+use crate::chunk::EcmascriptChunkPlaceable;
+
+#[turbo_tasks::function]
+pub async fn is_export_used(
+    module: ResolvedVc<Box<dyn EcmascriptChunkPlaceable>>,
+    export_name: RcStr,
+) -> Result<Vc<bool>> {
+}
