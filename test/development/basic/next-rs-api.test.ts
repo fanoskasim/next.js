b148a2eaecba1dbe4d6c581d7228cc0ab2f96264
Jiachi Liu <inbox@huozhi.im>
2025-04-16 15:16:32 +0200

    try catch app page _error

--- test/development/basic/next-rs-api.test.ts
+++ test/development/basic/next-rs-api.test.ts
@@ -243,6 +243,7 @@ describe('next.rs api', () => {
     expect(entrypoints.done).toBe(false)
     expect(Array.from(entrypoints.value.routes.keys()).sort()).toEqual([
       '/',
+      '/_error',
       '/_not-found',
       '/api/edge',
       '/api/nodejs',
