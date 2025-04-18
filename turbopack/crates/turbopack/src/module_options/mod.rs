05c9f631dbd2959b3f79b93c99d56b18aeb45e6a
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-04-18 12:49:42 -0700

    pass down

--- turbopack/crates/turbopack/src/module_options/mod.rs
+++ turbopack/crates/turbopack/src/module_options/mod.rs
@@ -147,6 +147,7 @@ impl ModuleOptions {
             execution_context,
             tree_shaking_mode,
             keep_last_successful_parse,
+            enable_intermediate_tree_shaking,
             ..
         } = *module_options_context.await?;
 
@@ -177,6 +178,7 @@ impl ModuleOptions {
             refresh,
             extract_source_map: matches!(ecmascript_source_maps, SourceMapsType::Full),
             keep_last_successful_parse,
+            enable_intermediate_tree_shaking,
             ..Default::default()
         };
         let ecmascript_options_vc = ecmascript_options.resolved_cell();
